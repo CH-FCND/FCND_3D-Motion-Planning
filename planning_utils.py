037c4dbfd85b7c77a16a60f1ae9e00c5ae124812
chaliburton <chris.haliburton@gmail.com>
2020-05-18 16:57:51 -0400

    updated epsilon to 2

--- planning_utils.py
+++ planning_utils.py
@@ -153,8 +153,8 @@ def heuristic(position, goal_position):
 
 def point(p):
     return np.array([p[0], p[1], 1.]).reshape(1, -1)
-    
-def collinearity_check(p1, p2, p3, epsilon=1e-6):   
+
+def collinearity_check(p1, p2, p3, epsilon=2        ):#e-6):   
     m = np.concatenate((p1, p2, p3), 0)
     det = np.linalg.det(m)
     return abs(det) < epsilon
